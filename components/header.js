3c50134bcf5bd0cab49d19f1eaba44dbd1e83939
Akash Bhave <akashbbhave@gmail.com>
2020-04-18 10:28:47 -0400

    Add all links to navigation bar

--- components/header.js
+++ components/header.js
@@ -1,6 +1,18 @@
 import React, { useState } from "react";
 import Link from "next/link";
 
+const NavLink = ({ href, title, end }) => (
+  <Link href={href}>
+    <a
+      className={`block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-dark ${
+        end || false ? "" : "mr-4"
+      }`}
+    >
+      {title}
+    </a>
+  </Link>
+);
+
 const Header = () => {
   const [isActive, setActive] = useState(false);
   return (
@@ -9,7 +21,9 @@ const Header = () => {
       className="flex items-center justify-between flex-wrap p-8 container mx-auto"
     >
       <div className="flex items-center flex-shrink-0 mr-8">
-        <span className="font-bold text-2xl tracking-tight">Project YCRO</span>
+        <Link href="/">
+          <a className="font-bold text-2xl tracking-tight">Project YCRO</a>
+        </Link>
       </div>
       <div className="block lg:hidden">
         <button
@@ -27,15 +41,15 @@ const Header = () => {
       <nav
         className={`w-full flex-grow lg:flex lg:items-center lg:w-auto ${
           isActive ? "block" : "hidden"
-        }`}
+        } `}
       >
         <div className="text-lg lg:flex-grow">
-          <div className="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-dark mr-4">
-            <Link href="/home">Home</Link>
-          </div>
-          <div className="block mt-4 lg:inline-block lg:mt-0 text-gray-700 hover:text-dark">
-            <Link href="/about">About</Link>
-          </div>
+          <NavLink href="/about" title="About" />
+          <NavLink href="/contact" title="Contact" end={true} />
+        </div>
+        <div className="text-lg">
+          <NavLink href="/ppe-request" title="Request PPE" />
+          <NavLink href="/ppe-donate" title="Donate PPE" end={true} />
         </div>
       </nav>
       <style jsx>{`
