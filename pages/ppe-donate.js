71cfbf6539f19e2bccb1de1c56bf8f4fc9d14827
Jason Klein <kleinjason234@gmail.com>
2020-04-22 20:13:49 -0400

    Update scroll behavior of iFrames

--- pages/ppe-donate.js
+++ pages/ppe-donate.js
@@ -1,23 +1,23 @@
 import SEO from "../components/seo";
 
 const PPEDonatePage = ({ data }) => (
-  <>
-    <SEO title="Donate PPE" />
-    <main className="container mx-auto">
-      <section className="p-8" style={{ height: "calc(100vh - 100px)" }}>
-        <iframe
-          style={{ height: "100%" }}
-          src="https://docs.google.com/forms/d/e/1FAIpQLSdsR-DkQwiTmll-QVtj4bvS4wR5asK6vX1a3DzlOGSduXX7Xg/viewform?usp=sf_link&embedded=true"
-          width="100%"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
-        >
-          Loading…
-        </iframe>
-      </section>
-    </main>
-  </>
+	<>
+		<SEO title="Donate PPE" />
+		<main className="container mx-full">
+			<section style={{ width: "100vw", height: "calc(100vh - 100px)" }}>
+				<iframe
+					style={{ height: "100%", width: "100vw" }}
+					src="https://docs.google.com/forms/d/e/1FAIpQLSdsR-DkQwiTmll-QVtj4bvS4wR5asK6vX1a3DzlOGSduXX7Xg/viewform?usp=sf_link&embedded=true"
+					width="100%"
+					frameBorder="0"
+					marginHeight="0"
+					marginWidth="0"
+				>
+					Loading…
+				</iframe>
+			</section>
+		</main>
+	</>
 );
 
 export default PPEDonatePage;
--- pages/ppe-request.js
+++ pages/ppe-request.js
@@ -1,23 +1,23 @@
 import SEO from "../components/seo";
 
 const PPERequestPage = ({ data }) => (
-  <>
-    <SEO title="Request PPE" />
-    <main className="container mx-auto">
-      <section className="p-8" style={{ height: "calc(100vh - 100px)" }}>
-        <iframe
-          style={{ height: "100%" }}
-          src="https://docs.google.com/forms/d/e/1FAIpQLSfDdXO8zSeozASqR8GUQDwsmXvUPbx1BGUveNh1Yu8fm7omuQ/viewform?usp=sf_link&embedded=true"
-          width="100%"
-          frameBorder="0"
-          marginHeight="0"
-          marginWidth="0"
-        >
-          Loading…
-        </iframe>
-      </section>
-    </main>
-  </>
+	<>
+		<SEO title="Request PPE" />
+		<main className="container">
+			<section style={{ width: "100vw", height: "calc(100vh - 100px)" }}>
+				<iframe
+					style={{ height: "100%" }}
+					src="https://docs.google.com/forms/d/e/1FAIpQLSfDdXO8zSeozASqR8GUQDwsmXvUPbx1BGUveNh1Yu8fm7omuQ/viewform?usp=sf_link&embedded=true"
+					width="100%"
+					frameBorder="0"
+					marginHeight="0"
+					marginWidth="0"
+				>
+					Loading…
+				</iframe>
+			</section>
+		</main>
+	</>
 );
 
 export default PPERequestPage;
