6c5c7fc81ab10fc404a0315aa22fd1698272998f
Jason Klein <kleinjason234@gmail.com>
2020-04-26 10:13:53 -0400

    Fix centering of header nav items

--- components/header.js
+++ components/header.js
@@ -74,7 +74,7 @@ const Header = () => {
 							}}
 						/>
 					</div>
-					<div className={`text-lg ${isActive ? "bg-light p-4" : ""}`} style={{ marginTop: "-1rem" }}>
+					<div className={`text-lg ${isActive ? "bg-light p-4 -mt-4" : ""}`}>
 						<NavLink
 							href="/ppe-request"
 							title="Request PPE"
