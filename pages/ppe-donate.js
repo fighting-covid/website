eef5fb436868dfa8a59b6a49c533c18861724c56
VarunChilukuri <48163435+VarunChilukuri@users.noreply.github.com>
2020-04-27 16:20:26 -0400

    fix overflow issues

--- pages/ppe-donate.js
+++ pages/ppe-donate.js
@@ -3,8 +3,8 @@ import SEO from "../components/seo";
 const PPEDonatePage = ({ data }) => (
 	<>
 		<SEO title="Donate PPE" />
-		<main className="container mx-full">
-			<section className="w-screen" style={{ overflowX : 'hidden', height: "calc(100vh - 100px)" }}>
+		<main className="container mx-full" style={{overflowX:"hidden"}}>
+			<section className="w-screen" style={{height: "calc(100vh - 100px)" }}>
 				<iframe
 					className="h-full"
 					src="https://docs.google.com/forms/d/e/1FAIpQLSdsR-DkQwiTmll-QVtj4bvS4wR5asK6vX1a3DzlOGSduXX7Xg/viewform?usp=sf_link&embedded=true"
