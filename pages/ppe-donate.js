e5fbc9701ab05b409537d26fb6e9705aa944fe3a
VarunChilukuri <48163435+VarunChilukuri@users.noreply.github.com>
2020-04-27 16:17:51 -0400

    hide overflowX and make it easier to access forms

--- pages/ppe-donate.js
+++ pages/ppe-donate.js
@@ -4,7 +4,7 @@ const PPEDonatePage = ({ data }) => (
 	<>
 		<SEO title="Donate PPE" />
 		<main className="container mx-full">
-			<section className="w-screen" style={{ height: "calc(100vh - 100px)" }}>
+			<section className="w-screen" style={{ overflowX : 'hidden', height: "calc(100vh - 100px)" }}>
 				<iframe
 					className="h-full"
 					src="https://docs.google.com/forms/d/e/1FAIpQLSdsR-DkQwiTmll-QVtj4bvS4wR5asK6vX1a3DzlOGSduXX7Xg/viewform?usp=sf_link&embedded=true"
