bed1e7b7b5c900ed80d93f7f345632d476fc1236
VarunChilukuri <48163435+VarunChilukuri@users.noreply.github.com>
2020-04-27 16:13:24 -0400

    set overflowX to hidden & make form length obvious

--- pages/ppe-request.js
+++ pages/ppe-request.js
@@ -4,7 +4,7 @@ const PPERequestPage = ({ data }) => (
 	<>
 		<SEO title="Request PPE" />
 		<main className="container">
-			<section className="w-screen" style={{ height: "calc(100vh - 100px)" }}>
+			<section className="w-screen" style={{ overflowX : 'hidden', height: "calc(100vh - 100px)" }}>
 				<iframe
 					className="h-full"
 					src="https://docs.google.com/forms/d/e/1FAIpQLSfDdXO8zSeozASqR8GUQDwsmXvUPbx1BGUveNh1Yu8fm7omuQ/viewform?usp=sf_link&embedded=true"
