5cb522222edcd2442d497fb4cf63fd9abcec5f89
VarunChilukuri <48163435+VarunChilukuri@users.noreply.github.com>
2020-04-27 16:52:33 -0400

    undo changes to center form

--- pages/contact.js
+++ pages/contact.js
@@ -10,7 +10,7 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import SEO from "../components/seo";
 
 const FormField = ({ title, type }) => (
-  <div className="mb-4", style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+  <div className="mb-4">
     <label className="text-lg block mb-2">{title}</label>
     <Field
       type={type}
