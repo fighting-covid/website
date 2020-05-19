f4acc40d3cf7e80b2327366afe5cd99c612469e7
VarunChilukuri <varunch321@gmail.com>
2020-05-19 12:19:44 -0400

    Add more organizations to the acknowledgments page

--- pages/acknowledgments.js
+++ pages/acknowledgments.js
@@ -1,7 +1,9 @@
 import Markdown from "react-markdown";
 import SEO from "../components/seo";
-import React from 'react'
-
+import React from 'react';
+import osms from '../assets/images/osms.jpg';
+import fcpl from '../assets/images/fcpl2.jpg';
+import covid from '../assets/images/3d.jpg';
 
 const Acknowledgments = ({ data }) => {
     return (
@@ -29,6 +31,45 @@ const Acknowledgments = ({ data }) => {
                         </div>
                         <Markdown className="markdown-body text-lg">
                             {"They provided face shields."}
+                        </Markdown>
+                    </div>
+                </section>
+                <section className="md:p-16 p-8 flex flex-wrap-reverse">
+                    <img src={covid}alt="3D for COVID" />
+                    <div className="md:pl-16 py-8 md:w-1/2 w-full flex flex-col md:justify-center justify-end">
+                        <div>
+                            <h2 className="font-serif font-bold md:text-4xl text-3xl heading">
+                                3D for COVID
+                            </h2>
+                        </div>
+                        <Markdown className="markdown-body text-lg">
+                            {"A blurb."}
+                        </Markdown>
+                    </div>
+                </section>
+                <section className="md:p-16 p-8 flex flex-wrap-reverse">
+                    <img src={osms} alt="Open Source COVID19 Medical Supplies" />
+                    <div className="md:pl-16 py-8 md:w-1/2 w-full flex flex-col md:justify-center justify-end">
+                        <div>
+                            <h2 className="font-serif font-bold md:text-4xl text-3xl heading">
+                            Open Source COVID19 Medical Supplies
+                            </h2>
+                        </div>
+                        <Markdown className="markdown-body text-lg">
+                            {"Blurb 2."}
+                        </Markdown>
+                    </div>
+                </section>
+                <section className="md:p-16 p-8 flex flex-wrap-reverse">
+                    <img src={fcpl} alt="Fairfax County Public Libraries" />
+                    <div className="md:pl-16 py-8 md:w-1/2 w-full flex flex-col md:justify-center justify-end">
+                        <div>
+                            <h2 className="font-serif font-bold md:text-4xl text-3xl heading">
+                            Fairfax County Public Libraries
+                            </h2>
+                        </div>
+                        <Markdown className="markdown-body text-lg">
+                            {"Blurb 3."}
                         </Markdown>
                     </div>
                 </section>
