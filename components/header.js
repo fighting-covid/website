67fb144fab244d3c1b06b72e701653782a1751b8
VarunChilukuri <varunch321@gmail.com>
2020-07-17 21:12:41 -0400

    Update navbar design with white line

--- components/header.js
+++ components/header.js
@@ -73,7 +73,7 @@ const Header = () => {
               }`}
           ></div>
           <div
-            className={`text-lg ${isActive ? "bg-dark p-4 pt-2 -mt-4" : ""}`}
+            className={`text-lg ${isActive ? "bg-light p-4 pt-2 -mt-4" : ""}`}
           ></div>
           <div className={`text-lg ${isActive ? "bg-dark p-4 -mt-4" : ""}`}>
             <NavLink
