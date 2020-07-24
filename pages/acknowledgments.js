180c82ea1f5c12db51019de3cf1d0e9a9c0dd6b8
VarunChilukuri <varunch321@gmail.com>
2020-07-24 13:11:36 -0400

    Set photos on Acknowledgments page to be in a row

--- pages/acknowledgments.js
+++ pages/acknowledgments.js
@@ -18,33 +18,6 @@ const Acknowledgments = ({ data }) => {
     <>
       <SEO title="Acknowledgments" />
       <main>
-        <section className="md:mb-16 relative p-8 md:p-64">
-          <img
-            className="w-full h-full bg-local object-cover bg-no-repeat bg-center absolute inset-0"
-            src={donate}
-            style={{ objectPosition: "50% 60%", filter: "grayscale(0)" }}
-          ></img>
-          <div className="p-4 flex flex-col items-left relative">
-            <div>
-              <p
-                className="text-left font-mono md:text-5xl text-4xl pb-4"
-                style={{ color: "#304352" }}
-              >
-                Our Mission
-              </p>
-            </div>
-            <div className="text-left font-mono lg:text-2xl text-xl">
-              <p style={{ color: "#304352" }}>
-                Healthcare workers are lacking basic personal protective
-                equipment (PPE) while fighting the COVID-19 pandemic, putting
-                themselves and their loved ones in danger. Help us deliver
-                critically needed supplies so they can safely continue saving
-                lives.
-              </p>
-            </div>
-          </div>
-        </section>
-
         <section className="p-8">
           <div className="flex justify-center">
             <h2
@@ -64,103 +37,103 @@ const Acknowledgments = ({ data }) => {
 
 
 
-        <section className="md:p-16 p-8 flex flex-wrap-reverse">
-          
-          
-          {/* 3dforcovid */}
-          <div
-            class="h-full flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img style={{ width: "100%" }} src={covid} alt="3D for COVID" />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
-              <a href="https://3dforcovid.com/" target="_blank">
-                <u>3D for COVID</u>
-              </a>
-            </p>
-          </div>
+        <section className="md:p-16 p-8">
+          <div className="flex flex-row">
 
+            {/* 3dforcovid */}
+            <div
+              class="w-1/4 mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img style={{ width: "100%" }} src={covid} alt="3D for COVID" />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://3dforcovid.com/" target="_blank">
+                  <u>3D for COVID</u>
+                </a>
+              </p>
+            </div>
 
-          {/* Fairfax County Public Libraries */}
-          <div
-            class="h-full flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img
-              style={{ width: "100%" }}
-              src={fcpl}
-              alt="Fairfax County Public Libraries"
-            />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF"}}>
-              <a href="https://www.fairfaxcounty.gov/library/" target="_blank">
-                <u>FCPL</u>
-              </a>
-            </p>
-          </div>
+            {/* Fairfax County Public Libraries */}
+            <div
+              class="w-1/2 mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img
+                style={{ width: "100%" }}
+                src={fcpl}
+                alt="Fairfax County Public Libraries"
+              />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://www.fairfaxcounty.gov/library/" target="_blank">
+                  <u>FCPL</u>
+                </a>
+              </p>
+            </div>
 
-
-          {/* Lets Breathe */}
-          <div
-            class="flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img style={{ width: "100%" }} src={breathe} alt="Lets Breathe" />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
-              <a
-                href="https://sites.google.com/view/lets-breathe/home?authuser=0"
-                target="_blank"
-              >
-                <u>Lets Breathe</u>
-              </a>
-            </p>
+            <div
+              class="w-1/4 column mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img
+                style={{ width: "100%" }}
+                src={breathe}
+                alt="Lets Breathe"
+              />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://sites.google.com/view/lets-breathe/home?authuser=0" target="_blank">
+                  <u>Lets Breathe</u>
+                </a>
+              </p>
+            </div>
           </div>
         </section>
 
 
         {/* Mika Naraynsingh */}
-        <section className="md:p-16 p-8 flex flex-wrap-reverse">
-          <div
-            class="h-full w-1/4 flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img src={mn} alt="Mika Naraynsingh" style={{ width: "100%" }} />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF"}}>
-              Mika Naraynsingh
+        <section className="md:p-16 p-8 flex ">
+          <div className="flex flex-row">
+            <div
+              class="w-1/4 mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img src={mn} alt="Mika Naraynsingh" style={{ width: "100%" }} />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                Mika Naraynsingh
             </p>
+            </div>
+
+
+            {/* Open Source Medical Supplies */}
+            <div
+              class="w-1/2 mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img
+                style={{ width: "100%" }}
+                src={osms}
+                alt="Open Source COVID19 Medical Supplies"
+              />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://opensourcemedicalsupplies.org/" target="_blank">
+                  <u>Open Source Medical Supplies</u>
+                </a>
+              </p>
+            </div>
+
+
+            {/* MasksforDocs */}
+            <div
+              class="w-1/4 mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              <img style={{ width: "100%" }} src={mfd} alt="Masks for Docs" />
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://masksfordocs.com" target="_blank">
+                  <u>Masks for Docs</u>
+                </a>
+              </p>
+            </div>
           </div>
-
-
-          {/* Open Source Medical Supplies */}
-          <div
-            class="h-full w-1/4 flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img
-              style={{ width: "100%" }}
-              src={osms}
-              alt="Open Source COVID19 Medical Supplies"
-            />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF"}}>
-              <a href="https://opensourcemedicalsupplies.org/" target="_blank">
-                <u>Open Source Medical Supplies</u>
-              </a>
-            </p>
-          </div>
-          
-
-          {/* MasksforDocs */}
-          <div
-            class="h-full w-1/4 flex-auto mx-16 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
-            style={{ background: "#304352" }}
-          >
-            <img style={{ width: "100%" }} src={mfd} alt="Masks for Docs" />
-            <p class="center" style={{ textAlign: "center", color: "#FFFFFF"}}>
-              <a href="https://masksfordocs.com" target="_blank">
-                <u>Masks for Docs</u>
-              </a>
-            </p>
-          </div>
-
         </section>
       </main>
     </>
