7cd08bb60e80b4a3ed5785c2b6aa9feab515b1b5
VarunChilukuri <varunch321@gmail.com>
2020-07-29 00:34:03 -0400

    Maintain original formatting on large screens on Sewing page

--- pages/ppe-make/sewing.js
+++ pages/ppe-make/sewing.js
@@ -23,7 +23,7 @@ const PPEMakeSewingPage = ({ data }) => {
 					<div className="md:flex justify-center">
 						<img className="ml-12 md:ml-0 w-3/4 md:w-1/2 border-solid border-4 border-gray-600 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-105 ..." src={materials_database} alt="Materials Database" />
 						<section className="md:ml-0 flex flex-col justify-center">
-							<div className="md:pr-1 pt-5">
+						<div className="md:pr-1 pt-5 md:pl-6 md:pr-4">
 								<h2
 									className="font-mono lg:text-2xl text-center text-xl"
 									style={{ color: "#304352" }}
@@ -31,7 +31,7 @@ const PPEMakeSewingPage = ({ data }) => {
 									Materials Database
                     			</h2>
 							</div>
-							<div className="text-center p-5">
+							<div className="p-5 md:text-left text-center">
 								<a href="https://3dprint.nih.gov/sites/default/files/models/3d_model_files/Prusa_Modified_TensionLocks_0.stl">
 									<button class="bg-dark hover:bg-blue-700 text-white py-2 px-4 rounded-full">
 										Download PDF
@@ -43,15 +43,15 @@ const PPEMakeSewingPage = ({ data }) => {
 					<div className="md:flex justify-center">
 						<img className="ml-12 md:ml-0 w-3/4 md:w-1/2 border-solid border-4 border-gray-600 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-105 ..." src={mask_instructions} alt="Mask Sewing Instructions" />
 						<section className="flex flex-col justify-center">
-							<div className="md:pr-1 pt-5">
+							<div className="md:pr-1 pt-5 md:pl-6 md:pr-4">
 								<h2
-									className="font-mono lg:text-2xl text-center text-xl"
+									className="font-mono lg:text-2xl md:text-left text-center text-xl"
 									style={{ color: "#304352" }}
 								>
 									Mask Sewing Instructions
                     			</h2>
 							</div>
-							<div className="p-5 text-center">
+							<div className="p-5 md:text-left text-center">
 								<a href="https://www.hopkinsmedicine.org/coronavirus/_documents/INF2003076_VW_Hand-Sewn%20Mask%20instructions-1.pdf">
 									<button class="bg-dark hover:bg-blue-700 text-white py-2 px-4 rounded-full">
 										Download PDF
