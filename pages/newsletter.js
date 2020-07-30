2b314f420dbe602523959f48efa9d545e296e135
Akash Bhave <akashbbhave@gmail.com>
2020-07-30 11:51:42 -0400

    Source newsletter page data from newsletter.yml

--- pages/newsletter.js
+++ pages/newsletter.js
@@ -1,9 +1,11 @@
+import yaml from "js-yaml";
+import fs from "fs";
+import path from "path";
+import Markdown from "react-markdown";
 import SEO from "../components/seo";
 import Link from "next/link";
 import React from "react";
 import ReactGA from "react-ga";
-import ear from "../assets/images/ear_saver.png";
-import virus from "../assets/images/virus.png";
 ReactGA.initialize("UA-167321699-1");
 ReactGA.pageview("/newsletter");
 
@@ -12,54 +14,45 @@ const Newsletter = ({ data }) => {
     <>
       <SEO title="Newsletter" />
       <main>
-        <section className="md:mb-8 relative md:p-8">
-          <div className="flex justify-center">
-            <h1 className="text-center heading uppercase">The YCRO Dispatch</h1>
+        <section className="container my-12">
+          <div className="text-center ">
+            <h1 className="heading uppercase">The YCRO Dispatch</h1>
           </div>
         </section>
-
-        <section className="flex md:mb-8 relative md:p-8 mx-64">
-          <div className="w-1/3 mr-8">
-            <img style={{ width: "100%" }} src={ear} alt="Zapppp" />
-          </div>
-          <div className="w-2/3">
-            <h2 className="heading">Volume 1</h2>
-            <p className="font-mono text-lg mb-4">
-              This newsletter covers Virginia's plans to reopen, how to safely
-              protest in the midst of a pandemic, and YCRO's innovative origmai
-              ear-saver design."
-            </p>
-            <button className="trans button">
-              <a href="/volume1.pdf" target="_blank">
-                Read More
-              </a>
-            </button>
-          </div>
-        </section>
-        <section className="flex md:mb-8 relative md:p-8 mx-64">
-          <div className="w-1/3 mr-8">
-            <img style={{ width: "100%" }} src={virus} alt="Zapppp" />
-          </div>
-
-          <div className="w-2/3">
-            <h2 className="heading">Volume 2</h2>
-            <p className="font-mono text-lg mb-4">
-              In this issue, learn about the science behind a virus and
-              SARS-CoV-2 in particular, delve into the behind the scenes of
-              YCRO's Outreach efforts, and find out more about the impact
-              private partnerships have made on slowing down testing and PPE
-              scarcity across the nation.
-            </p>
-            <button className="trans button">
-              <a href="/volume2.pdf" target="_blank">
-                Read More
-              </a>
-            </button>
-          </div>
+        <section className="container">
+          {data.volumes.map((volume, i) => (
+            <div className="flex flex-wrap mb-12" key={i}>
+              <div className="w-full lg:w-1/3 pr-8 mb-4">
+                <img
+                  src={require(`../assets/images/newsletter/volume-${
+                    i + 1
+                  }.png`)}
+                  alt={`Volume ${i + 1}`}
+                />
+              </div>
+              <div className="w-full lg:w-2/3">
+                <h2 className="heading">Volume {i + 1}</h2>
+                <Markdown className="text-lg markdown-body mb-4">
+                  {volume}
+                </Markdown>
+                <button className="trans button">
+                  <a href={`/newsletter/volume-${i + 1}.pdf`} target="_blank">
+                    Read More
+                  </a>
+                </button>
+              </div>
+            </div>
+          ))}
         </section>
       </main>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const dataPath = path.join(process.cwd(), "data/newsletter.yml");
+  const data = yaml.safeLoad(fs.readFileSync(dataPath));
+  return { props: { data } };
+}
 
 export default Newsletter;
