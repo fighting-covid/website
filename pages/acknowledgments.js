04b3181c6fa7805cfb8d946eb4de45636de6af84
revvu <reach.uveer@gmail.com>
2020-08-03 12:48:00 -0400

    revert to old acknowledgements

--- pages/acknowledgments.js
+++ pages/acknowledgments.js
@@ -1,10 +1,13 @@
-import yaml from "js-yaml";
-import fs from "fs";
-import path from "path";
 import Markdown from "react-markdown";
 import SEO from "../components/seo";
 import React from "react";
+import osms from "../assets/images/acknowledgments/osms.jpg";
+import fcpl from "../assets/images/acknowledgments/fcpl.jpg";
+import covid from "../assets/images/acknowledgements/3d-for-covid.jpg";
+import mfd from "../assets/images/acknowledgements/masks-for-docs.jpg";
+import mn from "../assets/images/acknowledgements/mika-naraynsingh.jpg";
 import donate from "../assets/images/donation.jpeg";
+import breathe from "../assets/images/acknowledgements/lets-breathe.jpg";
 import ReactGA from "react-ga";
 
 ReactGA.initialize("UA-167321699-1");
@@ -15,51 +18,254 @@ const Acknowledgments = ({ data }) => {
     <>
       <SEO title="Acknowledgments" />
       <main>
-        <section className="container my-12">
-          <div className="text-center">
-            <h1 className="heading">{data.title}</h1>
+        <section className="pr-8 pl-8 pt-8 pb-4 md:pb-8">
+          <div className="flex justify-center">
+            <h2
+              className="font-mono md:text-4xl text-3xl heading"
+              style={{ color: "black" }}
+            >
+              Acknowledgments
+            </h2>
           </div>
           <Markdown className="text-xl markdown-body mb-4 mx-auto text-center">
-            {data.subtitle}
+            {
+              "We would like to thank everyone who helped us deliver PPE to frontline workers."
+            }
           </Markdown>
         </section>
-        <section className="container">
-          <div className="flex flex-wrap items-center -mx-6">
-            {data.acknowledgments.map((ack, i) => (
+
+        {/* md and beyond */}
+        <div class="sm:hidden lg:block" >
+          <section className="md:pr-8 pr-8 md:pt-8 pt-8 md:pl-8 pl-8 md:pb-6 pb-0">
+            <div className="flex items-center flex-col md:flex-row">
+
+              {/* Mika Naraynsingh */}
               <div
-                className={`trans opacity-75 hover:opacity-100 cursor-pointer w-full sm:w-1/2 md:w-1/3 px-4 pb-8`}
-                key={i}
+                class="h-full w-3/4 md:w-4/12 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
               >
-                <div className="shadow hover:shadow-xl border border-dark-secondary rounded">
-                  <div className="flex items-center justify-center">
-                    <a href={ack.link} target="_blank">
-                      <img
-                        src={require(`../assets/images/acknowledgements/${ack.image}`)}
-                        alt={ack.title}
-                        className="w-full h-full object-cover"
-                      />
-                    </a>
-                  </div>
-                  <p
-                    className="text-light bg-dark text-center text-xl py-2"
-                    style={{ minHeight: "50px" }}
-                  >
-                    {ack.title}
-                  </p>
-                </div>
+                <img src={mn} alt="Mika Naraynsingh" style={{ width: "100%" }} />
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  Mika Naraynsingh
+              </p>
               </div>
-            ))}
+
+              {/* MasksforDocs */}
+              <div
+                class="h-full w-3/4 md:w-1/5 mx-16 my-5 md:my-0 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
+              >
+                
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  <a href="https://masksfordocs.com" target="_blank">
+                    <img style={{ width: "100%" }} src={mfd} alt="Masks for Docs" />
+                    Masks for Docs
+                  </a>
+                </p>
+              </div>
+
+              {/* Fairfax County Public Libraries */}
+              <div
+                class="h-full w-3/4 md:w-5/12 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
+              >
+                
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  <a href="https://www.fairfaxcounty.gov/library/" target="_blank">
+                    <img
+                    style={{ width: "100%" }}
+                    src={fcpl}
+                    alt="Fairfax County Public Libraries"
+                    />
+                    Fairfax County Public Libraries
+                  </a>
+                </p>
+              </div>
+
+            </div>
+          </section>
+        </div>
+
+        <div class= "sm:hidden lg:block">
+          <section className="md:p-8 pr-8 pl-8 pb-8 pt-4">
+          <div className="flex items-center flex-col md:flex-row">
+
+            {/* 3dforcovid */}
+            <div
+              class="h-full w-3/4 md:w-1/4 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://3dforcovid.com/" target="_blank">
+                  <img style={{ width: "100%" }} src={covid} alt="3D for COVID" />
+                  3D for COVID
+                </a>
+              </p>
+            </div>
+
+            {/* Open Source Medical Supplies */}
+            <div
+              class="h-full w-3/4 md:w-1/2 mx-16 my-5 md:my-0 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  
+                <a href="https://opensourcemedicalsupplies.org/" target="_blank">
+                    <img
+                    style={{ width: "100%" }}
+                    src={osms}
+                    alt="Open Source COVID19 Medical Supplies"
+                  />
+                  Open Source Medical Supplies
+                </a>
+              </p>
+            </div>
+
+            {/* Let's Breathe */}
+            <div
+              class="h-full w-3/4 md:w-1/4 column mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://sites.google.com/view/lets-breathe/home?authuser=0" target="_blank">
+                  <img
+                  style={{ width: "100%" }}
+                  src={breathe}
+                  alt="Lets Breathe"
+                />
+                  Let's Breathe
+                </a>
+              </p>
+            </div>
+
           </div>
         </section>
+        </div>
+
+        {/* sm to md */}
+        <div class="hidden sm:block lg:hidden" >
+          <section className="md:pr-8 pr-8 md:pt-8 pt-8 md:pl-8 pl-8 md:pb-6 pb-0">
+            <div className="flex items-center flex-col md:flex-row">
+
+              {/* Mika Naraynsingh */}
+              <div
+                class="h-full w-3/4 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
+              >
+                <img src={mn} alt="Mika Naraynsingh" style={{ width: "100%" }} />
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  Mika Naraynsingh
+              </p>
+              </div>
+
+              {/* MasksforDocs */}
+              <div
+                class="h-full w-3/4 mx-16 my-5 md:my-0 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
+              >
+                
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  <a href="https://masksfordocs.com" target="_blank">
+                    <img style={{ width: "100%" }} src={mfd} alt="Masks for Docs" />
+                    Masks for Docs
+                  </a>
+                </p>
+              </div>
+            </div>
+          </section>
+        </div>
+
+        <div class= "hidden sm:block lg:hidden">
+          <section className="md:p-8 pr-8 pl-8 pb-8 pt-4">
+          <div className="flex items-center flex-col md:flex-row">
+            
+            {/* 3dforcovid */}
+            <div
+              class="h-full w-3/4 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://3dforcovid.com/" target="_blank">
+                  <img style={{ width: "100%" }} src={covid} alt="3D for COVID" />
+                  3D for COVID
+                </a>
+              </p>
+            </div>
+
+            {/* Fairfax County Public Libraries */}
+            <div
+                class="h-full w-3/4 mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+                style={{ background: "#304352" }}
+              >
+                
+                <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  <a href="https://www.fairfaxcounty.gov/library/" target="_blank">
+                    <img
+                    style={{ width: "100%" }}
+                    src={fcpl}
+                    alt="Fairfax County Public Libraries"
+                    />
+                    Fairfax County Public Libraries
+                  </a>
+                </p>
+              </div>
+
+          </div>
+        </section>
+        </div>
+
+        <div class= "hidden sm:block lg:hidden">
+          <section className="md:pr-8 pr-8 md:pt-8 pt-8 md:pl-8 pl-8 md:pb-6 pb-0">
+            <div className="flex items-center flex-col md:flex-row">
+
+              {/* Open Source Medical Supplies */}
+            <div
+              class="h-full w-3/4 md:w-1/2 mx-16 my-5 md:my-0 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                  
+                <a href="https://opensourcemedicalsupplies.org/" target="_blank">
+                    <img
+                    style={{ width: "100%" }}
+                    src={osms}
+                    alt="Open Source COVID19 Medical Supplies"
+                  />
+                  Open Source Medical Supplies
+                </a>
+              </p>
+            </div>
+
+            {/* Let's Breathe */}
+            <div
+              class="h-full w-3/4 md:w-1/4 column mx-3 transition duration-500 ease-in-out box-shadow shadow-2xl transform hover:-translate-y-1 hover:scale-110 ..."
+              style={{ background: "#304352" }}
+            >
+              
+              <p class="center" style={{ textAlign: "center", color: "#FFFFFF" }}>
+                <a href="https://sites.google.com/view/lets-breathe/home?authuser=0" target="_blank">
+                  <img
+                  style={{ width: "100%" }}
+                  src={breathe}
+                  alt="Lets Breathe"
+                />
+                  Let's Breathe
+                </a>
+              </p>
+            </div>
+
+
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
 };
-
-export async function getStaticProps() {
-  const dataPath = path.join(process.cwd(), "data/acknowledgements.yml");
-  const data = yaml.safeLoad(fs.readFileSync(dataPath));
-  return { props: { data } };
-}
 
 export default Acknowledgments;
---
+++ pages/dev-acknowledgments.js
@@ -0,0 +1,65 @@
+import yaml from "js-yaml";
+import fs from "fs";
+import path from "path";
+import Markdown from "react-markdown";
+import SEO from "../components/seo";
+import React from "react";
+import donate from "../assets/images/donation.jpeg";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-167321699-1");
+ReactGA.pageview("/acknowledgments");
+
+const Acknowledgments = ({ data }) => {
+  return (
+    <>
+      <SEO title="Acknowledgments" />
+      <main>
+        <section className="container my-12">
+          <div className="text-center">
+            <h1 className="heading">{data.title}</h1>
+          </div>
+          <Markdown className="text-xl markdown-body mb-4 mx-auto text-center">
+            {data.subtitle}
+          </Markdown>
+        </section>
+        <section className="container">
+          <div className="flex flex-wrap items-center -mx-6">
+            {data.acknowledgments.map((ack, i) => (
+              <div
+                className={`trans opacity-75 hover:opacity-100 cursor-pointer w-full sm:w-1/2 md:w-1/3 px-4 pb-8`}
+                key={i}
+              >
+                <div className="shadow hover:shadow-xl border border-dark-secondary rounded">
+                  <div className="flex items-center justify-center">
+                    <a href={ack.link} target="_blank">
+                      <img
+                        src={require(`../assets/images/acknowledgements/${ack.image}`)}
+                        alt={ack.title}
+                        className="w-full h-full object-cover"
+                      />
+                    </a>
+                  </div>
+                  <p
+                    className="text-light bg-dark text-center text-xl py-2"
+                    style={{ minHeight: "50px" }}
+                  >
+                    {ack.title}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const dataPath = path.join(process.cwd(), "data/acknowledgements.yml");
+  const data = yaml.safeLoad(fs.readFileSync(dataPath));
+  return { props: { data } };
+}
+
+export default Acknowledgments;
