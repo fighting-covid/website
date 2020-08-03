3aee5bf92363679b69111ddd40161353fc8ee3e7
revvu <reach.uveer@gmail.com>
2020-08-03 15:36:46 -0400

    set up yml files for 3D printing

--- pages/ppe/make/3d.js
+++ pages/ppe/make/3d.js
@@ -3,6 +3,9 @@ import React from "react";
 import ReactGA from "react-ga";
 import Iframe from 'react-iframe'
 import Markdown from "react-markdown";
+import path from "path"
+import yaml from "js-yaml";
+import fs from "fs";
 
 /*
                 <div className="modelo-wrapper">
@@ -27,9 +30,19 @@ const PPEMake3DPage = ({ data }) => {
           <section className="container text-center my-5 md:my-12">
             <h1 className="heading">3D Printing</h1>
           </section>
+          
           <div className="flex flex-wrap flex-col md:flex-row">
-            <div className="flex flex-col px-4 md:px-0 h-64 md:w-1/2 w-full">
-              <Iframe url="https://platform.sayduck.com/public/preview/e40aec90-aea9-0138-6da4-3ea8e8560ebf"
+           
+           {data.stls.map((stl) => { 
+            const title = new String(stl.title);
+            const link = new String (stl.link.slice(0, -1));
+            const download = new String(stl.download.slice(0, -1));
+            const printTime = new String(stl.printTime);
+            const description = new String(stl.description);
+            return(
+            <>
+            <div className="mb-100 flex flex-col px-4 md:px-0 h-64 md:w-1/2 w-full">
+              <Iframe url={link}
                 width="100%"
                 height="200%"
                 id="myId"
@@ -39,21 +52,30 @@ const PPEMake3DPage = ({ data }) => {
             </div>
             <div className="flex flex-col">
               <section className="pl-2 md:pl-0 ml-3 md:ml-10">
-                <h3 className="heading text-2xl mt-2 sm:mt-0 mb-2">Face Shield</h3>
-                <p><i>Print time: 35 minutes</i></p>
-                <p className="mt-5 mb-4">This is the one sentence about this print. It is very strong and sturdy.</p>
-                <a href="https://3dprint.nih.gov/sites/default/files/models/3d_model_files/Prusa_Modified_TensionLocks_0.stl">
+                <h3 className="heading text-2xl mt-2 sm:mt-0 mb-2">{title}</h3>
+                <p><i>Print time: {printTime}</i></p>
+                <p className="mt-5 mb-4">{description}</p>
+                <a href={download}>
                   <button class="mb-3 md:mb-3 bg-dark hover:bg-blue-700 text-white py-2 px-4 rounded-full">
                     Download STL
                   </button>
                 </a>
               </section>
             </div>
+            </>
+            );
+          })}
           </div>
+
         </section>
       </main>
     </>
   );
 };
+export async function getStaticProps() {
+  const dataPath = path.join(process.cwd(), "data/3D.yml");
+  const data = yaml.safeLoad(fs.readFileSync(dataPath));
+  return { props: { data } };
+}
 
 export default PPEMake3DPage;
