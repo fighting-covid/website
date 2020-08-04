6c3b7db45d29d71338962b122b8fbb46c90ba682
VarunChilukuri <varunch321@gmail.com>
2020-08-04 15:09:54 -0400

    Update 3D printing info

--- pages/ppe/make/3d.js
+++ pages/ppe/make/3d.js
@@ -27,6 +27,8 @@ const PPEMake3DPage = ({ data }) => {
               const download = new String(stl.download.slice(0, -1));
               const printTime = new String(stl.printTime);
               const description = new String(stl.description);
+              const credit = new String(stl.credit);
+              const credit_link = new String(stl.credit_link.slice(0, -1));
               return (
                 <section className="flex flex-col md:flex-row mb-10">
                   <div className="mb-100 flex flex-col px-4 md:px-0 h-64 md:w-1/2 w-full">
@@ -42,6 +44,7 @@ const PPEMake3DPage = ({ data }) => {
                     <section className="pl-2 md:pl-0 ml-3 md:ml-10 md:w-5/6 mr-3 md:mr-0">
                       <h3 className="heading text-2xl mt-2 sm:mt-0 mb-2">{title}</h3>
                       <p><i>Print time: {printTime}</i></p>
+                      <p>Developed by <u><a target="_blank" style={{color: "blue"}}href={credit_link}>{credit}</a></u></p>
                       <p className="mt-5 mb-4">{description}</p>
                       <a href={download}>
                         <button class="mb-3 md:mb-3 bg-dark hover:bg-blue-700 text-white py-2 px-4 rounded-full">
