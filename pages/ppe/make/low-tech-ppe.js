24f41a407ae6e37687bedd5bb269dc2525b73505
Aryan34 <44515360+Aryan34@users.noreply.github.com>
2020-08-07 18:44:10 -0400

    Add PPE images and descriptions to low-tech page

--- pages/ppe/make/low-tech-ppe.js
+++ pages/ppe/make/low-tech-ppe.js
@@ -1,7 +1,9 @@
 import SEO from "../../../components/seo";
 import React from "react";
 import ReactGA from "react-ga";
-import ear_saver from "../../../assets/images/ear_saver.png";
+import paper_ear_saver from "../../../assets/images/make/innovative-design/paperearsaver.jpg";
+import card_ear_saver from "../../../assets/images/make/innovative-design/cardearsaver.png";
+import face_mask from "../../../assets/images/make/innovative-design/hairtiefacemask.png";
 
 ReactGA.initialize("UA-167321699-1");
 ReactGA.pageview("/ppe-make/low-tech-ppe");
@@ -14,7 +16,7 @@ const PPEMakeLowTechPPEPage = ({ data }) => {
           <h1 className="heading">Low Tech PPE</h1>
         </section>
 
-        <section className="flex flex-row md:justify-center justify-end">
+        <section className="flex flex-row md:justify-center justify-end mb-8">
           <div className="md:w-2/3 w-full mb-5 md:mb-0 md:pr-3">
             <div
               className="border-2 border-gray-500"
@@ -42,6 +44,57 @@ const PPEMakeLowTechPPEPage = ({ data }) => {
                 allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
               ></iframe>
+            </div>
+          </div>
+        </section>
+
+        <section className="flex flex-row md:justify-center justify-end">
+          <div className="md:w-2/3 w-full mb-5 md:mb-0 md:pr-3">
+            <div
+              className="border-2 border-gray-500"
+            >
+              <img
+                src={paper_ear_saver}
+                alt="ear saver"
+                className="w-full h-full object-cover"
+                style={{height: "300px"}}
+              />
+              <p>The hallmark of the Innovative Design team, origami ear savers are fast, easy, 
+                sturdy, cheap, comfortable, and recyclable. With 1 sheet of printer paper and 
+                just a few folds, you can create 3 ear savers! These can last for several days, 
+                if not weeks or months. They are essential if you plan on wearing a face mask 
+                for several hours.</p>
+            </div>
+          </div>
+
+          <div className="md:w-2/3 w-full mb-5 md:mb-0 md:pr-3">
+            <div
+              className="border-2 border-gray-500"
+            >
+              <img
+                src={card_ear_saver}
+                alt="ear saver"
+                className="w-full h-full object-cover"
+                style={{height: "300px"}}
+              />
+              <p>An ear saver may also be made with a credit card, playing card, Pokemon card, 
+                or Yu-Gi-Oh card. By cutting multiple notches, you can accommodate multiple 
+                sizes in one!</p>
+            </div>
+          </div>
+
+          <div className="md:w-2/3 w-full mb-5 md:mb-0 md:pr-3">
+            <div
+              className="border-2 border-gray-500"
+            >
+              <img
+                src={face_mask}
+                alt="ear saver"
+                className="w-full h-full object-cover"
+                style={{height: "300px"}}
+              />
+              <p>Using a square piece of cloth and two hair ties, you can fashion a face mask 
+                for yourself in just 3 easy steps!</p>
             </div>
           </div>
         </section>
